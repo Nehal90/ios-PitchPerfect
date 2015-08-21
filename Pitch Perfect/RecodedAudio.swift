51c839d118a840f7b925aa285439523e3bc59725
Nehal <Nehal@ANs-MacBook.local>
2015-08-20 21:00:01 -0400

    Pitch Perfect

---
+++ Pitch Perfect/RecodedAudio.swift
@@ -0,0 +1,9 @@
+//
+//  RecodedAudio.swift
+//  Pitch Perfect
+//
+//  Created by Nehal on 8/7/15.
+//  Copyright (c) 2015 WeiHo. All rights reserved.
+//
+
+import Foundation
---
+++ Pitch Perfect/RecordedAudio.swift
@@ -0,0 +1,18 @@
+//
+//  RecodedAudio.swift
+//  Pitch Perfect
+//
+//  Created by Nehal on 8/7/15.
+//  Copyright (c) 2015 WeiHo. All rights reserved.
+//
+
+import Foundation
+
+class RecordedAudio: NSObject
+{
+    var filePathUrl: NSURL!
+    var title: String!
+}
+    
+    
+
